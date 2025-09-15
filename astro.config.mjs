0294fd35abae6d83bdb599b742e0bfca2eafc731
YuminJo <nationmilo@gmail.com>
2025-09-15 22:33:44 +0900

    chore: 사이트 및 배포 관련 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: 'https://YuminJo.github.io/YuminJo-Blog/',
+	base: "/YuminJo-Blog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
